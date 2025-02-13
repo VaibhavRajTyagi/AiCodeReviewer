4b0d575557efd3097b4f4665bad1285183c0c789
VaibhavRT <vaibtyagi1610@gmail.com>
2025-02-13 23:12:46 +0530

    changed port

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const app = require('./src/app')
 
+const def_port = process.env.port || 3000;
 
-
-app.listen(3000, () => {
+app.listen(def_port, () => {
     console.log('Server is running on http://localhost:3000')
 })
