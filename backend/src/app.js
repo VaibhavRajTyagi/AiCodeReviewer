e5beaa8e43e8a7ac89637b6accbbab6f5fd75e56
VaibhavRT <vaibtyagi1610@gmail.com>
2025-02-14 01:30:32 +0530

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,7 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors({
-    origin: 'https://aicodereviewer-odi7.onrender.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors())
 
 
 app.use(express.json())
